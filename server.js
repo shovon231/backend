350963555da79acf033232e3b60f7aaee2126de7
shovon231 <shovon231@gmail.com>
2019-02-14 18:13:26 -0800

    Changes to be committed:

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.post("/post", upload.single("product-image"), (req, res) => {
         console.log("success")
         let responseBody = {
           status: true,
-          message: "successfuly insert data",
-          image: newName
+          message: "successfuly insert data"
+          //image: newName
         }
         db.close()
         res.send(JSON.stringify(responseBody))
@@ -46,37 +46,42 @@ app.post("/post", upload.single("product-image"), (req, res) => {
   })
 })
 
-app.post("/accountingr", function(req, res) {
-  console.log("**** inside in the retrive endpoint")
-  console.log("search", req.body)
-  MongoClient.connect(url, (err, db) => {
+// app.get("/", (req, res) => {
+//   res.send("goodbye world")
+// })
+
+app.get("/getItem", (req, res) => {
+  console.log("***** in the getReviews")
+  MongoClient.connect(url, function(err, db) {
+    console.log("connected")
     if (err) throw err
-    let dbo = db.db("mydb")
-    let query = {
-      category: searchBody
-    }
-    console.log("query", searchBody)
+    console.log("after error")
+    var dbo = db.db("mydb")
+    console.log("after dbo")
     dbo
-      .collection("accounting")
-      .find(query)
-      .toArray((err, result) => {
+      .collection("review")
+      .find({})
+      .toArray(function(err, result) {
         if (err) throw err
-        console.log("result", result)
-        if (result.length === 0) {
-          console.log("password didn't match!!")
-          res.send(JSON.stringify({ success: false }))
-          return
+        console.log(result)
+        let y = function(x) {
+          return x.category
         }
+        let x = result.map(y)
         let response = {
           status: true,
-          sid: genarateId()
+          images: x
         }
-        console.log("response: ", response)
+
         db.close()
-        res.send(JSON.stringify(result))
+        res.send(JSON.stringify(response))
       })
   })
 })
+
+// app.get("/get", function(res, req) {
+//   res.send("")
+// })
 
 app.use(bodyParser.raw({ type: "*/*" }))
 
